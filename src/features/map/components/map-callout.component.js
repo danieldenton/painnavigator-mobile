9d01994a266729f09de454e26eefe310b83946fe
stephenavocado <stephenwalter@chicagobooth.edu>
2022-02-28 01:43:56 +0000

    hello painnavigator

--- src/features/map/components/map-callout.component.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-import { CompactRestaurantInfo } from "../../../components/restaurant/compact-restaurant-info.component"
-
-export const MapCallout = ({ restaurant }) => {
-    return (
-        <CompactRestaurantInfo isMap restaurant={restaurant} />
-    )
-};
--- src/services/authentication/authentication.service.js
+++
@@ -1,5 +0,0 @@
-import firebase from 'firebase/compat/app';
-import 'firebase/compat/auth';
-
-export const loginRequest = (email, password) =>
-  firebase.auth().signInWithEmailAndPassword(email, password);
