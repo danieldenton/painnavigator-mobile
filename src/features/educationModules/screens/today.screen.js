db9d27219d31159ca03c06d7cc98cf346f981fe1
stephenavocado <stephenwalter@chicagobooth.edu>
2022-03-05 23:00:44 +0000

    EducationModuleCard component

--- src/features/educationModules/screens/today.screen.js
+++ src/features/educationModules/screens/today.screen.js
@@ -4,7 +4,8 @@ import { Text, TouchableOpacity } from "react-native";
 import { EducationModulesContext } from "../../../services/educationModules/education-modules.context";
 import { SafeArea } from "../../../components/utility/safe-area.component";
 import { Card } from "react-native-paper";
-import { educationModules } from "../data/education-module-data.json";
+
+import { EducationModuleCard } from "../components/education-module-card.component";
 
 const ModuleCard = styled(Card)`
     margin: ${(props) => props.theme.space[3]};
@@ -17,18 +18,7 @@ export const TodayScreen = ({ navigation }) => {
     
     return (
         <SafeArea>
-            <TouchableOpacity onPress={() => navigation.navigate("EducationUnit", 
-                {
-                    name,
-                    source,
-                })}> 
-                <ModuleCard>
-                    <Card.Content>
-                        <Text>{name}</Text>
-                        <Text>{length}</Text>
-                    </Card.Content>
-                </ModuleCard>
-            </TouchableOpacity>
+            <EducationModuleCard navigation={navigation} nextEducationModule={nextEducationModule} />
         </SafeArea>
     )
 };
