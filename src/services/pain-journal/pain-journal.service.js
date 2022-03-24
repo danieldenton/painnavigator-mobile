34c420a8dd6c1d40bcf703bd9a719d68ea99a2ed
stephenavocado <stephenwalter@chicagobooth.edu>
2022-03-24 18:45:08 +0000

    snakified object keys on the backend

--- src/services/pain-journal/pain-journal.service.js
+++ src/services/pain-journal/pain-journal.service.js
@@ -1,6 +1,5 @@
 import axios from 'axios';
 import camelize from "camelize";
-import snakify from "../../../utils";
 
 const baseUrl = 'https://3000-silver-slug-ggyxd3yy.ws-us38.gitpod.io';
 
@@ -14,44 +13,15 @@ export const getPainJournals = (setPainJournals, setPainJournalsLoaded) => {
 };
 
 export const patchPainJournal = (journalId, painJournal) => {
-
-    const {painScore, painSetting, painFeeling, whoWith, copingStrategies, otherNotes, painAfter } = painJournal;
-    
-    // TODO: if there is a way to do this with a function please let me know
-    const snakifiedJournal = {
-        pain_score: painScore,
-        pain_setting: painSetting,
-        pain_feeling: painFeeling,
-        who_with: whoWith,
-        coping_strategies: copingStrategies,
-        other_notes: otherNotes,
-        pain_after: painAfter
-    };
-
-    axios.patch(`${baseUrl}/api/v1/pain_journals/${journalId}`, { pain_journal: snakifiedJournal })
+    axios.patch(`${baseUrl}/api/v1/pain_journals/${journalId}`, { pain_journal: painJournal })
     .then((response) => {
         console.log(response.data);
     });
 };
 
 export const postPainJournal = (painJournal) => {
-
-    const {painScore, painSetting, painFeeling, whoWith, copingStrategies, otherNotes, painAfter } = painJournal;
-    
-    // TODO: if there is a way to do this with a function please let me know
-    const snakifiedJournal = {
-        pain_score: painScore,
-        pain_setting: painSetting,
-        pain_feeling: painFeeling,
-        who_with: whoWith,
-        coping_strategies: copingStrategies,
-        other_notes: otherNotes,
-        pain_after: painAfter
-    };
-
-    axios.post(`${baseUrl}/api/v1/pain_journals`, { pain_journal: snakifiedJournal })
+    axios.post(`${baseUrl}/api/v1/pain_journals`, { pain_journal: painJournal })
     .then((response) => {
         console.log(response.data);
     });
-    
 };
--- utils.js
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-export const snakify = (item) => {
-    return item.replace(/[A-Z]/g, letter => `_${letter.toLowerCase()}`);
-};
