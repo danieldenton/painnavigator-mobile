b8c6ec2e56eb792ec98e1d54b850ec6e885bed23
stephenavocado <stephenwalter@chicagobooth.edu>
2022-03-25 01:39:33 +0000

    firebase setup

--- src/features/account/screens/onboard.screen.js
+++ src/features/account/screens/onboard.screen.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { SafeArea } from "../../../components/safe-area.component";
-import { View, Text } from "react-native";
+import { Text } from "react-native";
 
 export const OnboardScreen = () => {
     return(
--- src/infrastructure/navigation/today.navigator.js
+++ src/infrastructure/navigation/today.navigator.js
@@ -2,7 +2,6 @@ import React from "react";
 
 import { createStackNavigator } from "@react-navigation/stack";
 
-
 import { TodayScreen } from "../../features/today/screens/today.screen";
 import { EducationUnitScreen } from "../../features/education/screens/education-unit.screen";
 import { MovementNavigator } from "./movement.navigator";
