6120b40b00ee3560e7c9e11f5b42b6fd731cac0e
stephenavocado <stephenwalter@chicagobooth.edu>
2022-03-27 00:40:07 +0000

    greeting component

--- src/features/today/screens/today.screen.js
+++ src/features/today/screens/today.screen.js
@@ -3,6 +3,7 @@ import { SafeArea } from "../../../components/safe-area.component";
 import { Text } from "react-native";
 
 import { TodayScreenHeader } from "../components/today-screen-header.component";
+import { Greeting } from "../components/greeting.component";
 import { EducationUnitCard } from "../../education/components/education-unit-card.component";
 import { MovementUnitCard } from "../../movement/components/movement-unit-card.component";
 import { DailyActivitiesTile } from "../../../components/daily-activities-tile.component";
@@ -12,13 +13,13 @@ import { EducationContext } from "../../../services/education/education.context"
 import { MovementContext } from "../../../services/movement/movement.context";
 
 export const TodayScreen = ({ navigation }) => {
-    const { userInfo } = useContext(ProfileContext);
+    const { name } = useContext(ProfileContext).userInfo;
     const { nextEducationModule } = useContext(EducationContext);
     const { currentMovementModule } = useContext(MovementContext);
 
     return (
         <SafeArea>
-            <Text>Good morning, {userInfo.name}</Text>
+            <Greeting timeOfDay={"Morning"} name={name} />
             <TodayScreenHeader headerName="TODAY'S EDUCATION"/>
             <EducationUnitCard 
                 navigation={navigation} 
