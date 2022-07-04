83989bc58e3cfeb6f5a3cd304120a22b7f0e9ebc
stephenavocado <stephenwalter@chicagobooth.edu>
2022-07-04 23:32:31 +0000

    company links component

--- src/components/side-menu/side-menu.component.js
+++ src/components/side-menu/side-menu.component.js
@@ -3,15 +3,7 @@ import { DrawerContainer, Logo } from "./side-menu.styles";
 import { MenuButtons } from "./side-menu-buttons.component";
 import { AltMenuButtons } from "./side-menu-alt-buttons.component";
 import { SafeAreaView, View } from "react-native";
-import { ButtonSection } from "../journals/journal.styles";
-import { LinkWrapper } from "../../features/account/components/terms-and-conditions.component";
-import styled from "styled-components";
-
-const LinkText = styled.Text`
-    font-family: Inter_300Light;
-    font-size: 14px;
-    color: #4056F4;
-`;
+import { CompanyLinks } from "./side-menu-company-links.component";
 
 export const SideMenu = ({ navigation }) => {
     return (
@@ -21,33 +13,7 @@ export const SideMenu = ({ navigation }) => {
                 <MenuButtons navigation={navigation} />
                 <View style={{ marginTop: 64 }}></View>
                 <AltMenuButtons navigation={navigation} />
-                <ButtonSection>
-                    <LinkWrapper
-                        onPress={() => navigation.navigate("Privacy")}
-                        style={{ marginLeft: 16 }}
-                    >
-                        <LinkText>
-                            Privacy Policy
-                        </LinkText>
-                    </LinkWrapper>
-                    <LinkWrapper
-                        onPress={() => navigation.navigate("Terms")}
-                        style={{ marginLeft: 16, marginTop: 12 }}
-                    >
-                        <LinkText>
-                            Terms of Use
-                        </LinkText>
-                    </LinkWrapper>
-                    <LinkWrapper
-                        style={{ marginLeft: 16, marginTop: 12 }}
-                    >
-                        <LinkText
-                            style={{ color: "black" }}
-                        >
-                            PainNavigator Version 0.0.1
-                        </LinkText>
-                    </LinkWrapper>
-                </ButtonSection>
+                <CompanyLinks navigation={navigation} />
             </DrawerContainer>
         </SafeAreaView>
     );
