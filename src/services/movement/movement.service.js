0172a24229966e698c99d366805caa871a650e90
stephenavocado <stephenwalter@chicagobooth.edu>
2022-12-30 01:33:22 +0000

    update post movementModuleCompletion to async await

--- src/services/movement/movement.service.js
+++ src/services/movement/movement.service.js
@@ -1,9 +1,11 @@
 import axios from 'axios';
 import { API_URL } from "@env"
 
-export const post = (module, uid) => {
-    axios.post(`${API_URL}/api/v1/movement_module_completions`, { movement_module: module, uid: uid })
-    .then((response) => {
-        const moduleData = response.data.data;
-    });
+export async function post(module, uid) {
+    try {
+        const response = await axios.post(`${API_URL}/api/v1/movement_module_completions`, { movement_module: module, uid: uid })
+        //console.log(response.data.data);
+    } catch (error) {
+        console.error(error);
+    }
 };
