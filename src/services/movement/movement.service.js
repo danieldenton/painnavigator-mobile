df7382c282183677885445568931ce201b129bea
stephenavocado <stephenwalter@chicagobooth.edu>
2022-12-30 03:01:28 +0000

    updated movement module service to set next movement module with response

--- src/services/movement/movement.service.js
+++ src/services/movement/movement.service.js
@@ -1,10 +1,16 @@
 import axios from 'axios';
 import { API_URL } from "@env"
+import { movementModules } from "../../features/movement/data/movement-modules-data.json";
 
-export async function post(module, uid) {
+export async function post(module, uid, setMovementProgress, setCurrentModule) {
     try {
         const response = await axios.post(`${API_URL}/api/v1/movement_module_completions`, { movement_module: module, uid: uid })
-        //console.log(response.data.data);
+        const data = response.data.data.attributes;
+        console.log(data);
+        const NEXT_MODULE_ID = data.module_id + 1;
+        setMovementProgress(NEXT_MODULE_ID);
+        const nextModule = movementModules.find(module => module.id === NEXT_MODULE_ID);
+        setCurrentModule(nextModule);
     } catch (error) {
         console.error(error);
     }
