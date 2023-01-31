bcf33040a76c2cf043d1495c8c83952b54e7fb23
Daniel Denton <danielmdenton@gmail.com>
2023-01-31 15:56:15 +0000

    beforeEach and afterEach added

--- src/features/today/test/todayScreen.test.js
+++ src/features/today/test/todayScreen.test.js
@@ -1,7 +1,12 @@
-import { render, screen } from "@testing-library/react-native";
+import { cleanup, screen } from "@testing-library/react-native";
 import { renderWithContext } from "../../../../test-utils";
 import React from "react";
 import { TodayScreen } from "../screens/today.screen";
+
+beforeEach(() => {
+    renderWithContext(<TodayScreen />)
+})
+afterEach(cleanup)
 
 // userOne has made some progress in the program
 jest.mock('react', () => ({
@@ -16,7 +21,7 @@ jest.mock('react', () => ({
 
 describe("renders today screen according to getUser call", () => {
     test("renders greeting", () => {
-        renderWithContext(<TodayScreen />)
+      
         
     }) 
 })
