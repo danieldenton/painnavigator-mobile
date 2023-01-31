641623ae8d3e8b6893c55dddedee96a929cb3ca3
Daniel Denton <danielmdenton@gmail.com>
2023-01-31 03:04:24 +0000

    expo-localization jest mock finally working

--- src/features/today/test/todayScreen.test.js
+++ src/features/today/test/todayScreen.test.js
@@ -1,18 +1,13 @@
 import { render, screen } from "@testing-library/react-native";
 import { renderWithContext } from "../../../../test-utils";
 import { TodayScreen } from "../screens/today.screen";
+import 'expo-localization';
+import { jestResetJsReanimatedModule } from "react-native-reanimated/lib/reanimated2/core";
 
-// jest.spyOn(timeZoneDateNumber).mockReturnValue('08')
-// jest.spyOn(DateTimeFormat, 'new').mockReturnValue('1/28/2023', '08')
 
-// Object.defineProperty(Intl.DateTimeFormat, 'new', {
-//     value: () => 1/30/2023
-// })
 
-// Object.defineProperty(Localization, 'timezone', {
-//     value: 'America/Los Angeles'
-//   })
-
+jest.mock('expo-localization', ()=> ({ timezone: "America/Los_Angeles"
+}))
 
 
 describe("renders today screen according to getUser call", () => {
