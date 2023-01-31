bcbc2651b2129c71214701b9d56adf7c16791a6e
Daniel Denton <danielmdenton@gmail.com>
2023-01-31 03:07:42 +0000

    expo-localization mock moved to jestSetupFile

--- jestSetupFile.js
+++ jestSetupFile.js
@@ -1,3 +1,6 @@
 jest.mock("@react-native-async-storage/async-storage", () =>
   require("@react-native-async-storage/async-storage/jest/async-storage-mock")
 );
+
+jest.mock('expo-localization', ()=> ({ timezone: "America/Los_Angeles"
+}))
--- src/features/today/test/todayScreen.test.js
+++ src/features/today/test/todayScreen.test.js
@@ -1,13 +1,6 @@
 import { render, screen } from "@testing-library/react-native";
 import { renderWithContext } from "../../../../test-utils";
 import { TodayScreen } from "../screens/today.screen";
-import 'expo-localization';
-
-
-
-jest.mock('expo-localization', ()=> ({ timezone: "America/Los_Angeles"
-}))
-
 
 describe("renders today screen according to getUser call", () => {
     test("renders greeting", () => {
