40ec2946b128cc18affb03e17daca6eb5392fe7f
Daniel Denton <danielmdenton@gmail.com>
2023-01-31 03:25:35 +0000

    done for the night. note added to test

--- src/features/today/test/todayScreen.test.js
+++ src/features/today/test/todayScreen.test.js
@@ -1,6 +1,9 @@
 import { render, screen } from "@testing-library/react-native";
 import { renderWithContext } from "../../../../test-utils";
+import React from "react";
 import { TodayScreen } from "../screens/today.screen";
+
+// import dont destructure useState without using the enzyme course fix 
 
 describe("renders today screen according to getUser call", () => {
     test("renders greeting", () => {
