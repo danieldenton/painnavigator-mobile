67f97ad5ae75ca47a76087aed62a5548d062ffa6
Daniel Denton <danielmdenton@gmail.com>
2023-01-31 03:05:04 +0000

    expo-localization jest mock finally working

--- src/features/today/test/todayScreen.test.js
+++ src/features/today/test/todayScreen.test.js
@@ -2,7 +2,6 @@ import { render, screen } from "@testing-library/react-native";
 import { renderWithContext } from "../../../../test-utils";
 import { TodayScreen } from "../screens/today.screen";
 import 'expo-localization';
-import { jestResetJsReanimatedModule } from "react-native-reanimated/lib/reanimated2/core";
 
 
 
