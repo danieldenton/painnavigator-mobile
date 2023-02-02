35fe7a95e57625f0e503143167741253373b5893
stephenavocado <stephenwalter@chicagobooth.edu>
2023-02-02 18:24:45 +0000

    update app version

--- src/components/side-menu/side-menu-company-links.component.js
+++ src/components/side-menu/side-menu-company-links.component.js
@@ -35,7 +35,7 @@ export const CompanyLinks = ({ navigation }) => {
                 <LinkText
                     style={{ color: "black" }}
                 >
-                    PainNavigator Version 1.4.5
+                    PainNavigator Version 1.4.6
                 </LinkText>
             </LinkWrapper>
         </ButtonSection>
