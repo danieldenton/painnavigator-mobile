4999e1160fe93bca9787a7991b295c46b17b6bb6
Daniel Denton <danielmdenton@gmail.com>
2023-02-17 23:38:43 +0000

    movementunit tracked

--- src/features/movement/components/video-info.component.js
+++ src/features/movement/components/video-info.component.js
@@ -2,12 +2,15 @@ import React, { useContext } from "react";
 import { Bookmark } from "../../../components/bookmark.component";
 import { BookmarkSection, Header, ModuleTypeTitle, UnitTitle, TitleSection } from "../../education/components/education-unit.styles";
 import { MovementContext } from "../../../services/movement/movement.context";
+import { MOVEMENT_UNIT_EVENTS } from "../../../amplitude-events";
 
 export const VideoInfo = () => {
     const { currentVideo } = useContext(MovementContext);
     const { id, name } = currentVideo;
     const { currentModule } = useContext(MovementContext);
     const { name: moduleName } = currentModule;
+
+    const trackEvent = MOVEMENT_UNIT_EVENTS.BOOKMARK_MOVEMENT_UNIT;
 
     return (
         <>
@@ -17,7 +20,7 @@ export const VideoInfo = () => {
                     <UnitTitle>{name}</UnitTitle>
                 </TitleSection>
                 <BookmarkSection>
-                    <Bookmark id={id} />
+                    <Bookmark id={id} trackEvent={trackEvent} />
                 </BookmarkSection>
             </Header>
         </>
