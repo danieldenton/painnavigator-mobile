3eea57f13887e9292503af8354d092028df50a96
Daniel Denton <danielmdenton@gmail.com>
2023-02-17 17:28:19 +0000

    react-testrender set to 17.0.2

--- src/features/account/tests/onboard.test.js
+++ src/features/account/tests/onboard.test.js
@@ -1,5 +1,5 @@
 import { render, screen } from "./onboardTest-utils"
-import userEvent from '@testing-library/user-event'
+import userEvent from "@testing-library/user-event"
 import { OnboardScreen } from "../screens/onboard.screen"
 import { ProviderCodeScreen } from "../screens/provider-code.screen"
 import { checkReferralCode } from "../../../services/authentication/authentication.service"
@@ -19,24 +19,24 @@ describe("renders onboard screen correctly", () => {
   })
 
   describe('renders screen with elements and submits provider code', () => {
-    beforeEach(() => {
-      render(<ProviderCodeScreen />)
-    })
     test("provider code screen renders header, directions, link, code input and disabled submit", () => {
-        const header = screen.getByText(/enter your referral code$/i)
-        const directions = screen.getByText(/to get one!$/i)
-        const link = screen.getByRole('button', 'painnavigator.io')
-        const input = screen.getByTestId("code-input")
-        const submitButton= screen.getByRole('button', /submit/i)
-        expect(header).toBeTruthy()
-        expect(directions).toBeTruthy()
-        expect(link). toBeTruthy()
-        expect(input).toBeTruthy()
-        expect(submitButton).toBeDisabled()
+      render(<ProviderCodeScreen />)
+      const header = screen.getByText(/enter your referral code$/i)
+      const directions = screen.getByText(/to get one!$/i)
+      const link = screen.getByRole('button', 'painnavigator.io')
+      const input = screen.getByTestId("code-input")
+      const submitButton= screen.getByRole('button', /submit/i)
+      expect(header).toBeTruthy()
+      expect(directions).toBeTruthy()
+      expect(link). toBeTruthy()
+      expect(input).toBeTruthy()
+      expect(submitButton).toBeDisabled()
     })
     test("provider code screen renders submit button which when pressed calls checkReferralCode", async () => {
+      user = userEvent.setup()
+      render(<ProviderCodeScreen />)
       const input = screen.getByTestId("code-input")
-      userEvent.type(input, {
+      user.type(input, {
         target: { value: "TEST12" }
       }) 
       const {rerender} = render(<ProviderCodeScreen />)
