f3ff65bf47b715184c877695b0045325487f90c3
Daniel Denton <danielmdenton@gmail.com>
2023-02-17 19:17:35 +0000

    userEvent and other dependencies removed

--- src/features/account/tests/onboard.test.js
+++ src/features/account/tests/onboard.test.js
@@ -1,5 +1,4 @@
 import { render, screen, fireEvent } from "./onboardTest-utils"
-// import userEvent from "@testing-library/user-event"
 import { OnboardScreen } from "../screens/onboard.screen"
 import { ProviderCodeScreen } from "../screens/provider-code.screen"
 
@@ -17,13 +16,13 @@ describe("renders onboard screen correctly", () => {
     })
   })
 
-  describe('renders screen with elements and submits provider code', () => {
+describe('renders screen with elements and submits provider code', () => {
     test("provider code screen renders header, directions, link, code input and disabled submit", () => {
       render(<ProviderCodeScreen />)
       const header = screen.getByText(/enter your referral code$/i)
       const directions = screen.getByText(/to get one!$/i)
       const link = screen.getByRole('button', 'painnavigator.io')
-      const input = screen.getByTestId("code-input")
+      const input = screen.getByTestId('code-input')
       const submitButton= screen.getByRole('button', /submit/i)
       expect(header).toBeTruthy()
       expect(directions).toBeTruthy()
