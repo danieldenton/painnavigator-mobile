94a36dec83f91bbae00ffe16485a9653e54fc4e2
Daniel Denton <danielmdenton@gmail.com>
2023-03-17 22:13:00 +0000

    bbreak

--- src/features/smart-goals/components/finished-goal.component.js
+++ src/features/smart-goals/components/finished-goal.component.js
@@ -1,47 +1,48 @@
 import { KeyboardView, GoalWrapper } from "./goal.styles"
 
-export const FinishedGoalComponent = ({ goal }) => {
+export const FinishedGoalComponent = ({ key }) => {
+    console.log(key)
 
-    const goalDetails = [
-        {
-            header: "Your SMART goal was:",
-            body: goal.goal,
-        },
-        {
-            header: "Your steps to work up to this goal were:",
-            body: goal.steps,
-            state: "steps"
-        },
-        {
-            header: "Your reward was:",
-            body: goal.reward
-        },
-        {
-            header: "the updates were",
-            body: goal.goal_updates
-        },
-        {
-            header: "What it meant to you:",
-            body: goal.meaning
-        },
-        {
-            header: "The challenges were:",
-            body: goal.challenges
-        }
-    ]
+    // const goalDetails = [
+    //     {
+    //         header: "Your SMART goal was:",
+    //         body: goal.goal,
+    //     },
+    //     {
+    //         header: "Your steps to work up to this goal were:",
+    //         body: goal.steps,
+    //         state: "steps"
+    //     },
+    //     {
+    //         header: "Your reward was:",
+    //         body: goal.reward
+    //     },
+    //     {
+    //         header: "the updates were",
+    //         body: goal.goal_updates
+    //     },
+    //     {
+    //         header: "What it meant to you:",
+    //         body: goal.meaning
+    //     },
+    //     {
+    //         header: "The challenges were:",
+    //         body: goal.challenges
+    //     }
+    // ]
 
-    const smartGoalDetails = goalDetails.map((goalDetail, idx) => {
-        return <GoalTextSection 
-                header={goalDetail.header} 
-                body={goalDetail.body} 
-                key={idx} 
-            />
-    })
+    // const smartGoalDetails = goalDetails.map((goalDetail, idx) => {
+    //     return <GoalTextSection 
+    //             header={goalDetail.header} 
+    //             body={goalDetail.body} 
+    //             key={idx} 
+    //         />
+    // })
 
     return (
         <KeyboardView>
                 <GoalWrapper>
-                    {smartGoalDetails}
+                    {/* {smartGoalDetails} */}
                 </GoalWrapper>
         </ KeyboardView>
     )
--- src/features/smart-goals/screens/finished-goal.screen.js
+++ src/features/smart-goals/screens/finished-goal.screen.js
@@ -3,15 +3,16 @@ import { SafeView } from "../../../components/safe-area.component";
 import { FinishedGoalComponent } from "../components/finished-goal.component";
 import { SmartGoalContext } from "../../../services/smart-goal/smart-goal.context";
 import { SMART_GOAL_EVENTS } from "../../../amplitude-events";
+import { useContext } from "react";
 
-export const FinishedGoalScreen = ({ navigation, goal }) => {
-    console.log(goal)
+export const FinishedGoalScreen = ({ navigation, key }) => {
+    const { finishedGoals } = useContext(SmartGoalContext)
 
     return (
         <SafeView>
             <NavigationBarLeft screen={"Smart Goal"} destination={"SmartGoalHome"} navigation={navigation} />
             <FinishedGoalComponent 
-                goal={goal} 
+                key={key}
                 navigation={navigation} 
              /> 
         </SafeView>
