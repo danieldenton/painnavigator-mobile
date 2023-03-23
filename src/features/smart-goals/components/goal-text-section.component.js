ffe94cd6d170cd38bfc1ac04f298904b892d367e
Daniel Denton <danielmdenton@gmail.com>
2023-03-23 18:23:06 +0000

    break

--- src/features/smart-goals/components/goal-text-section.component.js
+++ src/features/smart-goals/components/goal-text-section.component.js
@@ -7,6 +7,10 @@ const GoalTextWrapper = styled.View`
     margin-top: 16px;
 `;
 
+const UpdateGoalTextWrapper = styled.View`
+    margin-top: 8px;
+`;
+
 const Header = styled.Text`
     font-family: Inter_700Bold;
     font-size: 18px;
@@ -16,6 +20,7 @@ const Body = styled.Text`
     font-family: Inter_400Regular;
     font-size: 16px;
     margin-top: 4px;
+    border: black solid 1px;
 `;
 
 export const GoalTextSection = ({ edit, editing, header, body, state }) => {
@@ -46,11 +51,11 @@ export const UpdateGoalTextSection = ({ header, body }) => {
     })
 
     return (
-        <GoalTextWrapper>
+        <UpdateGoalTextWrapper>
             <Header>
                 {header}
             </Header>
             {updates}
-        </GoalTextWrapper>
+        </UpdateGoalTextWrapper>
     );
 };
