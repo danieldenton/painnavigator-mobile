0bee0553f7abcaf4d9a084491cf49f09ca1b8d03
Daniel Denton <danielmdenton@gmail.com>
2023-03-27 19:15:33 +0000

    keyboard scroll aware view added to mood journal primry thought

--- src/features/mood-journal/components/primary-thought.component.js
+++ src/features/mood-journal/components/primary-thought.component.js
@@ -1,18 +1,19 @@
 import React, { useContext } from "react";
+import { KeyboardAwareScrollView } from "react-native-keyboard-aware-scroll-view";
 import { JournalQuestion } from "../../../components/journal-question.component";
-import { MoodJournalContext } from "../../../services/mood-journal/mood-journal.context";
 import { TextInputLarge } from "../../../components/text-input.component";
+import { MoodJournalContext } from "../../../services/mood-journal/mood-journal.context";
 
 export const PrimaryThought = () => {
     const { changeEntry, currentPageData, moodJournal } = useContext(MoodJournalContext);
     
     return (
-        <>
+        <KeyboardAwareScrollView style={{ marginBottom: 120, marginRight: -16, paddingRight: 16 }}>
             <JournalQuestion question={currentPageData.question} helpText={currentPageData.helpText} />
             <TextInputLarge
                 value={moodJournal.primaryThought}
                 onChangeText={(change) => changeEntry(change, currentPageData.state)}    
             />
-        </>
+        </KeyboardAwareScrollView>
     );
 }; 
