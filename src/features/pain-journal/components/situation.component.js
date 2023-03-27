2523a5c24781469e6605075069606cd6d4c51116
Daniel Denton <danielmdenton@gmail.com>
2023-03-27 19:42:11 +0000

    updated pain journal situation to object standard and question and input component

--- src/features/pain-journal/components/situation.component.js
+++ src/features/pain-journal/components/situation.component.js
@@ -2,6 +2,7 @@ import React, { useContext } from "react";
 import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view';
 import { JournalQuestion } from "../../../components/journal-question.component";
 import { TextInputMedium } from "../../../components/text-input.component";
+import { QuestionAndInput } from "../../../components/question-and-input.component"
 import { PainJournalContext } from "../../../services/pain-journal/pain-journal.context";
 
 export const Situation = () => {
@@ -17,27 +18,36 @@ export const Situation = () => {
             accessibilityLabel: "situation-input"
         },
         {
-            question: "What will your reward be?",
-            helpText: "Be creative and pick something you really want! This could be a magazine subscription or a dinner out with friends.",
-            value: reward,
-            inputString: "reward",
-            accessibilityLabel: "reward-input"
+            question: "What were you feeling?",
+            helpText: "I was feeling anxious because I was running late.",
+            value: painJournal.feeling,
+            inputString: "feeling",
+            accessibilityLabel: "feeling-input"
+        },
+        {
+            question: "Who were you with?",
+            helpText: "I was by myself.",
+            value: painJournal.whoIWasWith,
+            inputString: "whoIWasWith",
+            accessibilityLabel: "whoIWasWith-label"
         }
+
     ]
 
     const questionsAndInputs = questions.map((question, idx) => {
-        const situations = [painJournal.situation, painJournal.feeling, painJournal.whoIWasWith]
-        const situationStrings = ["situation", "feeling", "whoIWasWith"]
-        return(
-            <>
-            <JournalQuestion question={question.question} helpText={question.helpText} key={idx} />
-            <TextInputMedium 
-                value={situations[idx]}
-                onChangeText={(change) => changeEntry(change, situationStrings[idx])}
-                key={idx} 
-            />
-            </>
-        )
+        // const situations = [painJournal.situation, painJournal.feeling, painJournal.whoIWasWith]
+        // const situationStrings = ["situation", "feeling", "whoIWasWith"]
+        return <QuestionAndInput question={question} input={changeEntry} idx={idx}/>
+        // return(
+        //     <>
+        //     <JournalQuestion question={question.question} helpText={question.helpText} key={idx} />
+        //     <TextInputMedium 
+        //         value={situations[idx]}
+        //         onChangeText={(change) => changeEntry(change, situationStrings[idx])}
+        //         key={idx} 
+        //     />
+        //     </>
+        // )
     })
 
     return(
