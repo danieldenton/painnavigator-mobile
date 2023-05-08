674de2e10453909b27d969e5a09385979e18e75b
Daniel Denton <danieldenton@Daniels-MBP-2.lan>
2023-05-08 15:30:13 -0700

    OutcomeAvtivity complet

--- src/features/completion/components/outcome-activity.component.js
+++ src/features/completion/components/outcome-activity.component.js
@@ -1,7 +1,19 @@
 import React, { useContext } from 'react'
 import { AuthenticationContext } from '../../../services/authentication/authentication.context'
+import { JournalQuestionAndIntensitySlider } from '../../../components/JournalQuestionAndIntensitySlider'
 
 export const OutcomeActivity = () => {
-    const { outcomeData, setOutcomeData } = useContext(AuthenticationContext)
+    const { outcomeData, changeOutcomeEntry } = useContext(AuthenticationContext)
+    const { outcomeActivityInterference } = outcomeData;
+
+    return (
+        <JournalQuestionAndIntensitySlider
+            question={"What number best describes how, during the past week, pain has interfered with your general activity?"}
+            helpText={"0 is not at all, 10 is pain has made normal activities impossible"}
+            value={outcomeActivityInterference}
+            onValueChange={changeOutcomeEntry}
+            state={"outcomeActivityInterference"}
+        />
+    );
 
 }
