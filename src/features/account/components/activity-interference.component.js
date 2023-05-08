7aa27ce78862276501351c83b6436e111b9ed609
Daniel Denton <danieldenton@Daniels-MBP-2.lan>
2023-05-08 15:12:22 -0700

    JournalQuestionAndIntensitySlider comp added to onboarding steps

--- src/features/account/components/activity-interference.component.js
+++ src/features/account/components/activity-interference.component.js
@@ -1,6 +1,5 @@
 import React, { useContext } from "react";
-import { IntensitySlider } from "../../../components/slider.component";
-import { JournalQuestion } from "../../../components/journal-question.component";
+import { JournalQuestionAndIntensitySlider } from "../../../components/JournalQuestionAndIntensitySlider";
 import { AuthenticationContext } from "../../../services/authentication/authentication.context";
 
 export const ActivityInterference = () => {
@@ -8,16 +7,12 @@ export const ActivityInterference = () => {
     const { activityInterference } = onboardingData;
 
     return (
-        <>
-            <JournalQuestion 
-                question={"What number best describes how, during the past week, pain has interfered with your general activity?"}
-                helpText={"0 is not commited at all, 10 is pain has made normal activities impossible"}
-            />
-            <IntensitySlider 
-                value={activityInterference} 
-                onValueChange={changeOnboardEntry} 
-                state={"activityInterference"}
-            />
-        </> 
+        <JournalQuestionAndIntensitySlider
+            question={"What number best describes how, during the past week, pain has interfered with your general activity?"}
+            helpText={"0 is not commited at all, 10 is pain has made normal activities impossible"}
+            value={activityInterference}
+            onValueChange={changeOnboardEntry}
+            state={"activityInterference"}
+        />
     );
 };
