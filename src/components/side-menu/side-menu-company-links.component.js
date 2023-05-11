df8daf88f155b54bfbf91713349d7972ef5c6bf6
Daniel Denton <danieldenton@Daniels-MBP-2.lan>
2023-05-10 18:39:49 -0700

    version 1.5.0

--- src/components/side-menu/side-menu-company-links.component.js
+++ src/components/side-menu/side-menu-company-links.component.js
@@ -35,7 +35,7 @@ export const CompanyLinks = ({ navigation }) => {
                 <LinkText
                     style={{ color: "black" }}
                 >
-                    PainNavigator Version 1.4.12
+                    PainNavigator Version 1.5.0
                 </LinkText>
             </LinkWrapper>
         </ButtonSection>
