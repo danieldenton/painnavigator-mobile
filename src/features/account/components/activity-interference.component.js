602603c2712d321bfe3bd6e3d964f31eeafd642c
Daniel Denton <danieldenton@Daniels-MBP-2.lan>
2023-05-15 12:32:32 -0700

    the word committed removed from onboarding question. minor fix. no new branch needed

--- src/features/account/components/activity-interference.component.js
+++ src/features/account/components/activity-interference.component.js
@@ -9,7 +9,7 @@ export const ActivityInterference = () => {
     return (
         <JournalQuestionAndIntensitySlider
             question={"What number best describes how, during the past week, pain has interfered with your general activity?"}
-            helpText={"0 is not commited at all, 10 is pain has made normal activities impossible"}
+            helpText={"0 is not at all, 10 is pain has made normal activities impossible"}
             value={activityInterference}
             onValueChange={changeOnboardEntry}
             state={"activityInterference"}
