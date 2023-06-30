67daaccea0767d6c77a9a178e6ea42266b8831a7
Daniel Denton <danieldenton@Daniels-MBP-2.lan>
2023-06-30 14:23:31 -0700

    helpers.js being combined with utils.js

--- src/utils.js
+++ src/utils.js
@@ -1,3 +1,33 @@
 import { Platform } from "react-native";
+import * as Localization from 'expo-localization';
+import { track } from '@amplitude/analytics-react-native';
 
 export const isAndroid = Platform.OS === "android";
+
+export const timeZone = Localization.timezone;
+export const todaysDate = new Date ();
+let options = { year: '2-digit', month: 'numeric', day: 'numeric', hour12: false, timeZone: timeZone }
+export const timeZonedTodaysDate = new Intl.DateTimeFormat('en-US', options).format(todaysDate)
+let optionsDayteNoYear = { month: 'numeric', day: 'numeric', timeZone: timeZone }
+export const foodJournalTimeZonedTodaysDate = new Intl.DateTimeFormat('en-US', {year: 'numeric', month: 'numeric', day: 'numeric', hour12: false, timeZone: timeZone }).format(todaysDate)
+
+export function formatDate(date_time_value) {
+    return date_time_value ? new Intl.DateTimeFormat('en-US', options).format(date_time_value) : null;
+};
+
+export function formatDateNoYear(date_time_value) {
+    return date_time_value ? new Intl.DateTimeFormat('en-US', optionsDayteNoYear).format(date_time_value) : null;
+};
+
+export function handleTrackEvent(trackEvent) {
+    if (trackEvent) {
+        track(trackEvent)
+    }
+}
+export function handleTrackExitEvent(trackExitEvent) {
+    if (trackExitEvent) {
+        track(trackExitEvent)
+    }
+}
+
+
