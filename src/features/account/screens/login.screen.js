644c14772b96230981430635d24f960a64cf25ea
Daniel Denton <danieldenton@Daniels-MBP-2.lan>
2023-07-12 13:24:25 -0700

    forgot password text on login screen

--- src/features/account/screens/login.screen.js
+++ src/features/account/screens/login.screen.js
@@ -2,17 +2,26 @@ import React, { useState, useContext } from "react";
 import { AuthenticationContext } from "../../../services/authentication/authentication.context";
 import { AuthTextInput, InputLabel } from "../../../components/text-input.component";
 import { JournalButton } from "../../../components/button.component";
-import { ErrorMessage, InputWrapper, SignUpOptions } from "../styles/account.styles";
+import { ErrorMessage, InputWrapper } from "../styles/account.styles";
 import { NavigationBarLeft } from "../../../components/journals/navigation-bar.component";
 import { SafeView } from "../../../components/safe-area.component";
 import { ActivityIndicator } from "../../../components/activity-indicator.component";
 import { track } from "@amplitude/analytics-react-native"
 import { ONBOARD_EVENTS } from "../../../amplitude-events";
+import styled from "styled-components/native";
 
 export const LoginScreen = ({ navigation }) => {
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
     const { onLogin, error, userLoading } = useContext(AuthenticationContext);
+
+    const ForgotPassword = styled.Text`
+        font-family: Inter_500Medium;
+        color: #4056F4;
+        font-size: 16px;
+        text-align: center;
+        margin-top: 300px;
+        `
 
     return(
         <SafeView>
@@ -45,6 +54,7 @@ export const LoginScreen = ({ navigation }) => {
                     onLogin(email, password)}}
             />
             {userLoading && <ActivityIndicator />}
+            <ForgotPassword>Forgot Password?</ForgotPassword>
         </SafeView>
     ); 
 };
