4fd0378cce33498c10d907ba47826564a73fdcc6
Daniel Denton <danieldenton@Daniels-MBP-2.lan>
2023-08-14 13:04:28 -0700

    break

--- src/features/today/components/dashboard-styles.js
+++ src/features/today/components/dashboard-styles.js
@@ -9,7 +9,7 @@ export const styles = StyleSheet.create({
     },
     modalBackground: {
         flex: 1,
-        backgroundColor: 'rgba(0, 0, 0, 0.25)',
+        backgroundColor: 'rgba(0, 0, 0, 0.25)'
       },
     modalContainer: {
         position: 'relative',
