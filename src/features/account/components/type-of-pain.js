82d0aa30ee080318ff6b4b4af64fd71748813fe8
Daniel Denton <danieldenton@Daniels-MBP-2.lan>
2023-08-17 10:28:05 -0700

    pain injections copied to type of pain for structure

---
+++ src/features/account/components/type-of-pain.js
@@ -0,0 +1,34 @@
+import React from 'react'
+import { JournalQuestion } from '../../../components/journal-question.component'
+import { SingleSelectCheckBox } from "../../../components/checkbox/single-select-checkbox.component";
+import { View } from 'react-native';
+import { injectionsAndSurgery } from "./../data/onboard-data.json"
+
+export const PainInjections = ({ onValueChange, data }) => {
+    const add = (optionId) => {
+        onValueChange(optionId, "painInjections")
+    };
+    
+    const options = injectionsAndSurgery.map((option) => {
+        return (
+            <SingleSelectCheckBox 
+                add={add}
+                key={option.id}
+                optionData={option} 
+                selectedOption={data.painInjections}
+            />            
+        );
+    });
+
+    return (
+        <>
+            <JournalQuestion 
+                question={"Are you currently undergoing pain injections as part of your treatment plan?"} 
+            />
+                <View style={{ marginTop: 20, marginBottom: 140 }}>
+                    {options}
+                </View>
+        </>
+    )
+    
+}
