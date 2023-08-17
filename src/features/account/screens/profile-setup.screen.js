ab21d3768fc5ae80195ce392c72cff66ae149d67
Daniel Denton <danieldenton@Daniels-MBP-2.lan>
2023-08-17 15:39:40 -0700

    console.log removed

--- src/features/account/screens/profile-setup.screen.js
+++ src/features/account/screens/profile-setup.screen.js
@@ -76,7 +76,7 @@ export const ProfileSetupScreen = ({ navigation }) => {
             navigation.navigate("Register")
         }   
     }
-console.log(onboardingData.typeOfPain)
+
     return(
         <SafeView>
             <NavigationBarLeft 
