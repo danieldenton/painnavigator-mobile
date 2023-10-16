f585b9148477fe4b811c75f6dfd43dea26631fe1
Daniel Denton <danieldenton@Daniels-MBP-2.lan>
2023-10-16 11:58:20 -0700

    updated bookmark compoenen t haptics

--- src/components/bookmark.component.js
+++ src/components/bookmark.component.js
@@ -2,25 +2,32 @@ import React, { useContext } from "react";
 import { TouchableOpacity } from "react-native";
 import { BookmarkIcon, BookmarkedIcon } from "../icons";
 import { BookmarksContext } from "../services/bookmarks/bookmarks.context";
-import * as Haptics from 'expo-haptics';
-import { track } from '@amplitude/analytics-react-native'
+import * as Haptics from "expo-haptics";
+import { track } from "@amplitude/analytics-react-native";
 import { MovementContext } from "../services/movement/movement.context";
 
 export const Bookmark = ({ id, trackEvent }) => {
-    const { bookmarks, addToBookmarks, removeFromBookmarks, isMovement } = useContext(BookmarksContext);
-    const { saveMovementModule } = useContext(MovementContext)
-    const isBookmarked = bookmarks.find((b) => b === id)
+  const { bookmarks, addToBookmarks, removeFromBookmarks, isMovement } =
+    useContext(BookmarksContext);
+  const { saveMovementModule } = useContext(MovementContext);
+  const isBookmarked = bookmarks.find((b) => b === id);
 
-    return (
-        <TouchableOpacity
-            onPress={() => {
-                isMovement ? saveMovementModule() :
-                !isBookmarked ? (addToBookmarks(id), track(trackEvent)) : removeFromBookmarks(id);
-                Haptics.impactAsync(Haptics.ImpactFeedbackStyle.Light)
-            }}
-            testID={"bookmark"}
-        >
-           {isBookmarked ? <BookmarkedIcon /> : <BookmarkIcon />} 
-        </TouchableOpacity>
-    );
+  console.log(isMovement)
+
+  return (
+    <TouchableOpacity
+      onPress={() => {
+        Haptics.impactAsync(Haptics.ImpactFeedbackStyle.Light)
+        isMovement
+          ? saveMovementModule()
+          : !isBookmarked
+          ? (addToBookmarks(id), track(trackEvent))
+          : removeFromBookmarks(id);
+          
+      }}
+      testID={"bookmark"}
+    >
+      {isBookmarked ? <BookmarkedIcon /> : <BookmarkIcon />}
+    </TouchableOpacity>
+  );
 };
