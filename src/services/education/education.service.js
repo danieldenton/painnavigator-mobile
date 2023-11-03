d7ffa634748a34920f1b00699a71019c1f6d583f
Daniel Denton <danieldenton@Daniels-MBP-2.attlocal.net>
2023-11-03 08:36:57 -0700

    commented out unuses post education modulle completions response

--- src/services/education/education.service.js
+++ src/services/education/education.service.js
@@ -4,7 +4,7 @@ import { API_URL } from "@env"
 export async function postEducationModule(module, uid) {
     try {
         const response = await axios.post(`${API_URL}/api/v2/education_module_completions`, { education_module_completion: module, uid: uid })
-        const data = response.data.data.attributes;
+        // const data = response.data.data.attributes;
         // TODO get this from the back end
         // setEducationProgress(data.education_progress);
     } catch (error) {
