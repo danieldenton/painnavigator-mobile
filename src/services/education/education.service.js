3e374c1c711f6958d3642faec84ef879f76b4249
Daniel Denton <danieldenton@Daniels-MBP-2.attlocal.net>
2023-11-03 08:23:45 -0700

    deleted console logs

--- src/services/education/education.service.js
+++ src/services/education/education.service.js
@@ -2,7 +2,6 @@ import axios from 'axios';
 import { API_URL } from "@env"
 
 export async function postEducationModule(module, uid) {
-    // console.log(module)
     try {
         const response = await axios.post(`${API_URL}/api/v2/education_module_completions`, { education_module_completion: module, uid: uid })
         const data = response.data.data.attributes;
