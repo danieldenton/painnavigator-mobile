dc8c1ff28782e3e0300064e48c9f96a7aa81ea8d
Daniel Denton <danieldenton@Daniels-MBP-2.attlocal.net>
2023-11-03 08:55:18 -0700

    deleted today screen console.log

--- src/features/today/screens/today.screen.js
+++ src/features/today/screens/today.screen.js
@@ -120,7 +120,6 @@ export const TodayScreen = ({ navigation }) => {
     getMoodJournals(uid, setMoodJournals);
     getFoodJournals(uid, setFoodJournals);
   }, []);
-  console.log(uid)
 
   useEffect(() => {
     if (lastDateOnApp !== timeZonedTodaysDate) {
