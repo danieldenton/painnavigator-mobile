b2f01d27f867e67d7c60bb86be9364d2819f30d0
Daniel Denton <danieldenton@Daniels-MBP-2.attlocal.net>
2023-11-03 08:13:14 -0700

    deleted unused setOrientation in educationContext

--- src/services/education/education.context.js
+++ src/services/education/education.context.js
@@ -42,17 +42,6 @@ export const EducationContextProvider = ({ children }) => {
         setSkippedEducationModules(prevSkipped => prevSkipped.filter(unit => unit !== unitId));
     };
 
-    //const setOrientation = () => {
-        //if (Dimensions.get('window').height > Dimensions.get('window').width) {
-            //Device is in portrait mode, rotate to landscape mode.
-            //ScreenOrientation.lockAsync(ScreenOrientation.OrientationLock.LANDSCAPE_RIGHT);
-        //}
-        //else {
-            //Device is in landscape mode, rotate to portrait mode.
-            //ScreenOrientation.lockAsync(ScreenOrientation.OrientationLock.PORTRAIT);
-        //}
-    //};
-
     const skipModule = () => {
         const module = {
             module_id: currentModule.id,
