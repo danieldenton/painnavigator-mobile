070e2ca1f4604a9c7f42aff188e1d30ec7c2fa5c
Daniel Denton <danieldenton@Daniels-MBP-2.attlocal.net>
2023-11-20 16:56:18 -0800

    updated screen orientation lock in movement module completion screen useEffect

--- src/features/movement/screens/completion-screen.js
+++ src/features/movement/screens/completion-screen.js
@@ -1,12 +1,17 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect } from "react";
 import { ModuleButton } from "../../../components/button.component";
 import { ButtonSection } from "../../../components/journals/journal.styles";
 import { MovementContext } from "../../../services/movement/movement.context";
 import { SafeView } from "../../../components/safe-area.component";
 import { CompletionComponent } from "../components/completion.component";
+import * as ScreenOrientation from "expo-screen-orientation"
 
 export const CompletionScreen = ({ navigation, completionMessage }) => {
   const { resetModule } = useContext(MovementContext);
+
+  useEffect(() => {
+    ScreenOrientation.lockAsync(ScreenOrientation.OrientationLock.PORTRAIT)
+  }, [])
 
   return (
     <SafeView>
