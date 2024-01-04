9f0d66809f367063ceb44c380ac3001d3204c571
Daniel Denton <danielmdenton@gmail.com>
2024-01-04 15:24:42 -0800

    updated postEducationModule in service only

--- src/services/education/education.service.js
+++ src/services/education/education.service.js
@@ -1,12 +1,14 @@
 import axios from 'axios';
 import { API_URL } from "@env"
 
-export async function postEducationModule(module, uid) {
+export async function postEducationModule(uid, module, status) {
     try {
-        const response = await axios.post(`${API_URL}/api/v2/education_module_completions`, { education_module_completion: module, uid: uid })
-        // const data = response.data.data.attributes;
-        // TODO get this from the back end
-        // setEducationProgress(data.education_progress);
+        const response = await axios.post(`${API_URL}/api/v2/education_module_completions`, { 
+            uid: uid, 
+            education_module_completion: module, 
+            status: status
+        })
+       return response
     } catch (error) {
         console.error(error);
     }
