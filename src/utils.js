145489a4e877a282a38dc1748f7e4c1819fd0dfd
Daniel Denton <danielmdenton@gmail.com>
2024-01-15 17:38:19 -0800

    setObjectState created in utils.js

--- src/utils.js
+++ src/utils.js
@@ -53,6 +53,14 @@ export function formatDateNoYear(date_time_value) {
     : null;
 }
 
+export const setObjectState = (value, key, setObject) => {
+  setObject((object) => ({
+    ...object,
+    [key]: value,
+  }));
+};
+
+
 export function handleTrackEvent(trackEvent) {
   if (trackEvent) {
     track(trackEvent);
