52b89e3ca6ff1e701bf31b2f2f86db65a5d35dee
Daniel Denton <danielmdenton@gmail.com>
2024-01-17 15:52:17 -0800

    setPainJournalnow in alignment with intensitySlider

--- src/features/pain-journal/components/intensity-after.component.js
+++ src/features/pain-journal/components/intensity-after.component.js
@@ -4,15 +4,15 @@ import { JournalQuestion } from "../../../components/journal-question.component"
 import { PainJournalContext } from "../../../services/pain-journal/pain-journal.context";
 
 export const IntensityAfter = () => {
-    const { changeEntry, currentPageData, painJournal } = useContext(PainJournalContext);
+    const { currentPageData, painJournal, setPainJournal } = useContext(PainJournalContext);
     const { question } = currentPageData;
 
     return(
         <>
             <JournalQuestion question={question} />
             <IntensitySlider 
-                onValueChange={changeEntry} 
-                state={"intensityAfter"} 
+                setState={setPainJournal} 
+                objectKey={"intensityAfter"} 
                 value={painJournal.intensityAfter} 
             />
         </>
