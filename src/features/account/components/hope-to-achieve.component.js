57f6a8bffa11327acd5bc21f185edff4cce19347
Daniel Denton <danielmdenton@gmail.com>
2024-01-16 16:49:33 -0800

    onboarding updated to HopesToAchieve

--- src/features/account/components/hope-to-achieve.component.js
+++ src/features/account/components/hope-to-achieve.component.js
@@ -2,53 +2,58 @@ import React, { useContext } from "react";
 import { View } from "react-native";
 import { JournalQuestion } from "../../../components/journal-question.component";
 import { AuthenticationContext } from "../../../services/authentication/authentication.context";
-import { MultiSelectCheckBox, MultiSelectScroll } from "../../../components/multi-select-checkbox.component";
-import { hopesOptions } from "./../data/onboard-data.json"
+import {
+  MultiSelectCheckBox,
+  MultiSelectScroll,
+} from "../../../components/multi-select-checkbox.component";
+import { hopesOptions } from "./../data/onboard-data.json";
 
 export const HopeToAchieve = () => {
-    const { onboardingData, setOnboardingData} = useContext(AuthenticationContext);
-    const { hopesToAchieve } = onboardingData;
+  const { onboardingData, setOnboardingData } = useContext(
+    AuthenticationContext
+  );
+  const { hopesToAchieve } = onboardingData;
 
-    const selectedHopes = hopesToAchieve;
+  const selectedHopes = hopesToAchieve;
 
-    const add = (optionId) => {
-        setOnboardingData(object => ({
-            ...object,
-            ["hopesToAchieve"]: [...selectedHopes, optionId]
-        }));
-    };
-    
-    const remove = (optionId) => {
-        const newHopes = selectedHopes.filter(
-          (x) => x !== optionId
-        );
-        setOnboardingData(entry => ({
-            ...entry,
-            ["hopesToAchieve"]: newHopes
-        }));    
-    };
+  const add = (optionId) => {
+    setOnboardingData((object) => ({
+      ...object,
+      ["hopesToAchieve"]: [...selectedHopes, optionId],
+    }));
+  };
 
-    const hopes = hopesOptions.map((option) => {
-        return (
-            <MultiSelectCheckBox 
-                add={add}
-                key={option.id}
-                optionData={option} 
-                remove={remove}
-                selectedOptions={selectedHopes}
-            />            
-        );
-    });
+  const remove = (optionId) => {
+    const newHopes = selectedHopes.filter((x) => x !== optionId);
+    setOnboardingData((entry) => ({
+      ...entry,
+      ["hopesToAchieve"]: newHopes,
+    }));
+  };
 
-    return(
-        <>
-            <JournalQuestion 
-                question={"What do you hope to achieve by completing PainNavigator?"} helpText={"Tap the circle to select. You can select more then one of these."} />
-            <MultiSelectScroll>
-                <View style={{ marginTop: 10, marginBottom: 60 }}>
-                    {hopes}
-                </View>
-            </MultiSelectScroll>
-        </> 
+  const hopes = hopesOptions.map((option) => {
+    return (
+      <MultiSelectCheckBox
+        add={add}
+        key={option.id}
+        optionData={option}
+        remove={remove}
+        selectedOptions={selectedHopes}
+      />
     );
+  });
+
+  return (
+    <>
+      <JournalQuestion
+        question={"What do you hope to achieve by completing PainNavigator?"}
+        helpText={
+          "Tap the circle to select. You can select more then one of these."
+        }
+      />
+      <MultiSelectScroll>
+        <View style={{ marginTop: 10, marginBottom: 60 }}>{hopes}</View>
+      </MultiSelectScroll>
+    </>
+  );
 };
