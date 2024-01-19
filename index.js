e9efdbc48d873dd5b0c1a867ba71236fb5cf7983
Daniel Denton <danielmdenton@gmail.com>
2024-01-18 16:53:41 -0800

    deleted comments in metro config

--- index.js
+++ index.js
@@ -3,9 +3,6 @@ import * as Sentry from '@sentry/react-native';
 
 import App from './App';
 
-// registerRootComponent calls AppRegistry.registerComponent('main', () => App);
-// It also ensures that whether you load the app in Expo Go or in a native build,
-// the environment is set up appropriately
 registerRootComponent(App);
 
 export default Sentry.wrap(App);
