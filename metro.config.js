85dd584152ed2f13fd0a4874d7d75d9ea7cded4d
Daniel Denton <danielmdenton@gmail.com>
2024-01-18 16:38:17 -0800

    new metro config per the expo docs

--- metro.config.js
+++ metro.config.js
@@ -3,17 +3,3 @@ const { getSentryExpoConfig } = require('@sentry/react-native/metro');
 const config = getSentryExpoConfig(__dirname);
 
 module.exports = config;
-// const { getDefaultConfig, mergeConfig } = require("@expo/metro-config");
-// const {createSentryMetroSerializer} = require('@sentry/react-native/dist/js/tools/sentryMetroSerializer');
-
-// const config = {
-//   serializer: {
-//     customSerializer: createSentryMetroSerializer(),
-//   },
-// };
-
-// const defaultConfig = getDefaultConfig(__dirname);
-
-// defaultConfig.resolver.assetExts.push("cjs");
-
-// module.exports = mergeConfig(getDefaultConfig(__dirname), config);
