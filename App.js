3c73cc8299e6200c9dfa474d24ae422a18490089
Daniel Denton <danielmdenton@gmail.com>
2024-01-23 11:24:01 -0800

    Sentry restored to latest

--- App.js
+++ App.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect, useRef } from "react";
 import firebase from "firebase/compat/app";
 import "firebase/compat/auth";
 import { LogBox } from "react-native";
-import * as Sentry from "sentry-expo";
+import * as Sentry from '@sentry/react-native';
 import { init } from "@amplitude/analytics-react-native";
 import { AMPLITUDE_API_KEY } from "@env";
 import * as Notifications from "expo-notifications";
