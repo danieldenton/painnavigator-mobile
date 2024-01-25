0abb77f9d2f98bf022899c52da701fd9fbb34800
Daniel Denton <danielmdenton@gmail.com>
2024-01-25 15:22:48 -0800

    pn-intro-unit.component should be complete

--- src/features/education/components/pn-intro-unit.component.js
+++ src/features/education/components/pn-intro-unit.component.js
@@ -2,21 +2,30 @@ import React, { useContext } from "react";
 import { EducationContext } from "../../../services/education/education.context";
 import { EducationUnitInfo } from "./education-unit-info.component";
 import { Scroll } from "../../../components/scroll.component";
+import { Summary } from "./education-unit.styles";
 import { BulletList } from "../../../components/bullet-list.component";
 import { View } from "react-native";
 
 export const TextUnit = () => {
-    const { currentModule, educationIntroStep, setEducationIntroStep } = useContext(EducationContext);
-    const { pnIntroData } = currentModule;
+  const { currentModule, educationIntroStep, setEducationIntroStep } =
+    useContext(EducationContext);
+  const { pnIntroData } = currentModule;
 
-    return (
-        <>
-            <Scroll style={{ paddingLeft: 16, paddingRight: 16, marginBottom: 100 }}>
-                <View style={{ marginBottom: 60 }}>
-                    <EducationUnitInfo id={id} name={name} summary={summary} type={type} />
-                    <BulletList bullets={steps}/>
-                </View>
-            </Scroll>
-        </>
-    );
+  return (
+    <>
+      <Scroll style={{ paddingLeft: 16, paddingRight: 16, marginBottom: 100 }}>
+        <View style={{ marginBottom: 60 }}>
+          <EducationUnitInfo
+            id={educationIntroStep}
+            name={pnIntroData[educationIntroStep].name}
+            summary={pnIntroData[educationIntroStep].summary}
+            type={"text"}
+          />
+          {pnIntroData[educationIntroStep].summary_2 ? <Summary summary={pnIntroData[educationIntroStep].summary_2} /> : null}
+          <BulletList bullets={pnIntroData[educationIntroStep].steps} />
+          {pnIntroData[educationIntroStep].summary_3 ? <Summary summary={pnIntroData[educationIntroStep].summary_3} /> : null}
+        </View>
+      </Scroll>
+    </>
+  );
 };
