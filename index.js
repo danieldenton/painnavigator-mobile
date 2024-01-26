b66e1dcf5f5fd985df192296dfc61feece7b9fd5
Daniel Denton <danielmdenton@gmail.com>
2024-01-25 16:23:44 -0800

    updated education-unit-screen. total reorganization. last commit notes were lost

--- index.js
+++
@@ -1,8 +0,0 @@
-import { registerRootComponent } from 'expo';
-import * as Sentry from '@sentry/react-native';
-
-import App from './App';
-
-registerRootComponent(App);
-
-export default Sentry.wrap(App);
