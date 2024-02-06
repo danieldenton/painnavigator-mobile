62a59649e219469b43532a50c13e20588ebb3c45
Daniel Denton <danielmdenton@gmail.com>
2024-02-06 08:25:44 -0800

    custumTour set to 4 as prop passed to compOnTop for WellnessCoackReminder

--- src/features/today/components/wellness-coach-reminder.component.js
+++ src/features/today/components/wellness-coach-reminder.component.js
@@ -6,7 +6,6 @@ import { DashboardTourComponentOnTop } from "../../dashboard-tour/dashboard-tour
 import { styles } from "../../dashboard-tour/dashboard-styles";
 
 export const WellnessCoachReminder = ({ visible, setVisible, navigation }) => {
-
   const handleFinish = () => {
     setTour(null);
     setVisible(false);
@@ -17,17 +16,21 @@ export const WellnessCoachReminder = ({ visible, setVisible, navigation }) => {
       <View style={styles.container}>
         <Modal animationType="slide" transparent={true} visible={visible}>
           <View style={styles.modalBackground}>
-              <DashboardTourComponentOnTop />
+            <DashboardTourComponentOnTop />
             <View
               style={[
                 styles.modalContainer,
                 { marginTop: tourObj[4].tourTextBubble },
               ]}
             >
-                <View
-                  style={styles.triangleRightTop}
-                />
-              <Text style={styles.modalContent}>{tourObj[customTour]?.text}</Text>
+              <View style={styles.triangleRightTop} />
+              <Text style={styles.modalContent}>
+                Text: Ready to meet with your personal Wellness Coach? Click
+                here. Collaborating with your coach is one of the keys to
+                achieving your goals - your coach is a real person, there to
+                answer questions, support you, and help you overcome obstacles
+                in your pain and your life.
+              </Text>
               <ModuleButton
                 onPress={() => navigation.navigate("WellnessCoach")}
                 title={"CHECK IN WITH YOUR COACH"}
