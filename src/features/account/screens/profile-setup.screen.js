b86500847edff9ef9019b5dc4f6cd1547989b7d0
Daniel Denton <danielmdenton@gmail.com>
2024-03-27 16:35:22 -0700

    breaking

--- src/features/account/screens/profile-setup.screen.js
+++ src/features/account/screens/profile-setup.screen.js
@@ -93,7 +93,6 @@ export const ProfileSetupScreen = ({ navigation }) => {
           disabled={onboardPages[step].disabled}
           title={"Next"}
           onPress={() => {
-            console.log(step)
             step === 6
               ? (handleEducationProgram(), navigation.navigate("Register"))
               // : step === 9 && onboardingData.typeOfPain !== "Low Back Pain"
