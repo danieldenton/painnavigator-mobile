3b99f300d0dc42c0d55026d2913d7def044c68d9
Daniel Denton <96963270+danieldenton@users.noreply.github.com>
2024-04-25 09:17:41 -0700

    unnecassary patchOutcomeData arg removed

--- src/services/outcome.context.js
+++ src/services/outcome.context.js
@@ -16,7 +16,7 @@ export const OutcomeContextProvider = ({ children }) => {
   });
   const [completedProgram, setCompletedProgram] = useState(false);
 
-  const patchCompletedProgram = async (uid, outcomeData) => {
+  const patchCompletedProgram = async (uid) => {
     try {
       await axios.patch(`${API_URL}/api/v2/users/${uid}`, {
         completed_program: true,
@@ -35,7 +35,7 @@ export const OutcomeContextProvider = ({ children }) => {
   };
 
   const completeProgram = () => {
-    patchCompletedProgram(uid, outcomeData);
+    patchCompletedProgram(uid);
     setCompletedProgram(true);
   };
 
