44d68285515f82dbb5eb657d15f0a26000b1339b
Daniel Denton <96963270+danieldenton@users.noreply.github.com>
2024-04-29 09:04:04 -0700

    AppUpdateRequired component created. In Progress

---
+++ src/features/today/components/app_update_required.component.js
@@ -0,0 +1,49 @@
+import React, { useContext } from "react";
+import { View, Text, Modal } from "react-native";
+import { ModuleButton } from "../../../components/button.component";
+import { tourObj } from "../../dashboard-tour/data/dashboard-tour-data";
+import { DashboardTourComponentOnTop } from "../../dashboard-tour/dashboard-tour-comp-top";
+import { styles } from "../../dashboard-tour/dashboard-styles";
+import { AuthenticationContext } from "../../../services/authentication.context";
+import { isAndroid } from "../../../utils";
+
+export const AppUpdateRequired = ({ navigation }) => {
+  const { uid } = useContext(AuthenticationContext);
+
+  const handleAppUpdateRequired = () => {
+    // patchAppUpdateRequireds
+    // setAppUpdateRequired(false)
+    // link to app stores
+  };
+
+  return (
+    <>
+      <View style={styles.container}>
+        <Modal
+          animationType="slide"
+          transparent={true}
+          visible={!appUpdateRequired}
+        >
+          <View style={styles.modalBackground}>
+            <DashboardTourComponentOnTop tour={4} />
+            <View
+              style={[
+                styles.modalContainer,
+                { marginTop: tourObj[4].tourTextBubble },
+              ]}
+            >
+              <View style={styles.triangleRightTop} />
+              <Text style={styles.modalContent}>
+                We've made some changes. Please update your PainNavigator app to procedd.
+              </Text>
+              <ModuleButton
+                onPress={() => handleAppUpdateRequired()}
+                title={"UPDATE NOW"}
+              />
+            </View>
+          </View>
+        </Modal>
+      </View>
+    </>
+  );
+};
