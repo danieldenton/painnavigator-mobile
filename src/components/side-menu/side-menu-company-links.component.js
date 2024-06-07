599359eb3025e80c61c3e07c4f931d8dc9c55c19
Daniel Denton <96963270+danieldenton@users.noreply.github.com>
2024-06-07 11:22:29 -0700

    updated version codes

--- src/components/side-menu/side-menu-company-links.component.js
+++ src/components/side-menu/side-menu-company-links.component.js
@@ -23,7 +23,7 @@ export const CompanyLinks = ({ navigation }) => {
       </LinkWrapper>
       <LinkWrapper style={{ marginTop: 12 }}>
         <LinkText style={{ color: "black" }}>
-          PainNavigator Version 2.0.5
+          PainNavigator Version 2.0.6
         </LinkText>
       </LinkWrapper>
     </ButtonSection>
